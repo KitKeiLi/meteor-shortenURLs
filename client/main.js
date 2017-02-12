16503ad58478bdf134ce0965d7bd0e7390b4db83
Kit Kei Li <kitli@ucdavis.edu>
2017-02-12 15:38:57 -0800

    adds validating URLs

---
+++ client/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Header from './components/header';
+import LinkCreate from './components/link_create';
+import { Links } from '../imports/collections/links';
+
+const App = () => {
+  return (
+    <div>
+        <Header/>
+        <LinkCreate/>
+    </div>
+  );
+};
+
+Meteor.startup( () => {
+   ReactDom.render(<App/>, document.querySelector('.render-target'));
+});
